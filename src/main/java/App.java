7ffd3a10c1876da3e82f42c47950385deeed18d1
EricM321 <ericm321@live.com>
2021-01-27 20:18:28 +0000

    Added task 2 to main app entry point

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,24 @@
 import task1.BimBam;
+import task2.CustomMapImpl;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
+
+    private static void HashMapTask(){
+        CustomMapImpl myMap = new CustomMapImpl();
+        System.out.println("\nMap size after init: " + myMap.size());
+
+        myMap.put(1,2);
+        System.out.println("Map size after myMap.put(1,2): " + myMap.size());
+
+        System.out.println("Map return after myMap.get(1): " + myMap.get(1));
+
+        System.out.println("Map return after myMap.remove(1): " + myMap.remove(1));
+        System.out.println("Map size after myMap.remove(1): " + myMap.size());
+
+    }
 
     public static void main(String[] args){
 
@@ -24,7 +39,8 @@ public class App {
                     bimBam.BimBam();
                     System.out.println("\n");
                 } else if(enteredValue == 2){
-                    System.out.println("\n");
+                    HashMapTask();
+                    System.out.println("\nThe CustomMapImpl test cover more.\n");
                 } else if(enteredValue == 3){
                     System.out.println("\n");
                 } else if(enteredValue == 0){
